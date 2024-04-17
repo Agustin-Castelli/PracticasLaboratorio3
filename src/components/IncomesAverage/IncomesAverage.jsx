c309c0d7d72c06353535e187c7b521d77122ba83
Agustin Castelli <rev0.c4chomax@gmail.com>
2024-04-17 00:27:51 -0300

    Practica Obligatoria 2

--- src/components/IncomesAverage/IncomesAverage.jsx
+++ src/components/IncomesAverage/IncomesAverage.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 const IncomesAverage = ({netIncomes}) => {
 
-    const incomesMapped = netIncomes.map((netIncome) => {
+    const incomesMapped = netIncomes.map((netIncome) => (
         netIncome.income
-    });
+    ));
 
     const sum = incomesMapped.reduce((acc, current) => acc + current, 0);
     const average = sum / incomesMapped.length;
